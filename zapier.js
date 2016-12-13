ced9fcb8daaeea56f961a8ca6a76044df5e6537c
Hampus Persson <hampus.persson@apsis.com>
2016-12-13 05:36:23 +0100

    test: add test for apsis_get_active_events_pre_poll method

--- zapier.js
+++ zapier.js
@@ -1,242 +1,239 @@
 const deepAssign = require('deep-assign');
 
 const Zap = {
-    apsis_send_transactional_email_copy_pre_write: function (bundle) {
-        var actionFields = bundle.action_fields;
-        var data = {};
-        var demDataFields = [];
-        for (var prop in actionFields) {
-            if (actionFields.hasOwnProperty(prop)) {
-                if (prop === "Key") {
-                    var keys = actionFields[prop];
-                    var index = 0;
-                    for (var pr in keys) {
-                        if (demDataFields.length - 1 < index) {
-                            throw new ErrorException('The number of Demographic Data fields can not be greater than the number of values.');
-                        }
-                        demDataFields[pr].Key = keys[pr];
-                        index++;
-                    }
-                }
-                else if (prop === "Value") {
-                    var values = actionFields[prop];
-                    for (var p in values) {
-                        var newObj = { Value: values[p], Key: "" };
-                        demDataFields.push(newObj);
-                    }
-                }
-                else {
-                    data[prop] = actionFields[prop];
-                }
+  apsis_send_transactional_email_copy_pre_write: function (bundle) {
+    var actionFields = bundle.action_fields;
+    var data = {};
+    var demDataFields = [];
+    for (var prop in actionFields) {
+      if (actionFields.hasOwnProperty(prop)) {
+        if (prop === "Key") {
+          var keys = actionFields[prop];
+          var index = 0;
+          for (var pr in keys) {
+            if (demDataFields.length - 1 < index) {
+              throw new ErrorException('The number of Demographic Data fields can not be greater than the number of values.');
             }
+            demDataFields[pr].Key = keys[pr];
+            index++;
+          }
         }
-        if (demDataFields.length > 0) {
-            for (var i = 0; i < demDataFields.length; i++) {
-                if (demDataFields[i].Key === "") {
-                    throw new ErrorException('The number of values can not be greater than the number of Demographic Data fields.');
-                }
-            }
-            data.demDataFields = demDataFields;
-        }
-        bundle.request.data = JSON.stringify(data);
-        return bundle.request;
-    },
-
-    remove_subscriber_from_optoutall_post_write: function (bundle) {
-        // Working
-        var response = JSON.parse(bundle.response.content);
-        if (response.Message === 'Subscriber e-mail address does not exist on the Opt-out all list') {
-            bundle.response.status_code = 200;
-        }
-        console.log(response.Message);
-        return bundle.response;
-    },
-
-    remove_subscriber_from_optoutall_pre_write: function (bundle) {
-        // Working
-        bundle.request.method = 'DELETE';
-        var data = bundle.action_fields_full.email;
-        bundle.request.data = JSON.stringify(data);
-        return bundle.request;
-    },
-
-
-    apsis_new_mailinglist_subscriber_post_poll: function (bundle) {
-        // Working
-        var data = JSON.parse(bundle.response.content);
-        if (data.Code != 1) {
-            throw new ErrorException('Unable to retrieve the subscribers on the mailinglist');
+        else if (prop === "Value") {
+          var values = actionFields[prop];
+          for (var p in values) {
+            var newObj = { Value: values[p], Key: "" };
+            demDataFields.push(newObj);
+          }
         }
         else {
-            var pages = parseInt(data.Result.TotalPages, 10);
-            console.log(typeof (pages));
-            if (pages > 1) {
-                var request = {
-                    'method': 'GET',
-                    'url': 'https://se.api.anpdm.com:8443/v1/mailinglists/' + bundle.trigger_fields.MailinlistID + '/subscribers/' + pages + '/100',
-                    'params': {},
-                    'headers': {
-                        'accept': 'application/json',
-                        'content-type': 'application/json; charset=utf-8'
-                    },
-                    'auth': bundle.request.auth,
-                    'data': null
-                };
-
-                var response = JSON.parse(z.request(request));
-                data = response.content;
-            }
-            bundle.response.content = Subscribers;
-
-            return bundle.response;
+          data[prop] = actionFields[prop];
         }
-    },
-
-    apsis_new_mailinglist_subscriber_pre_poll: function (bundle) {
-        // Working
-        return bundle.request;
-    },
-
-    apsis_update_event_attendee_pre_write: function (bundle) {
-        // Working
-        bundle.request.method = 'PUT';
-        var data = bundle.action_fields_full.Status;
-        bundle.request.data = JSON.stringify(data);
-        return bundle.request;
-    },
-
-    apsis_find_attendee_pre_search: function (bundle) {
-        // Working
-        var data = { EventId: bundle.search_fields.eventId };
-        bundle.request.method = 'POST';
-        bundle.request.data = JSON.stringify(data);
-        return bundle.request;
-    },
-
-    apsis_get_event_attendee_pre_poll: function (bundle) {
-        // Working
-        var data = {
-            EventId: bundle.trigger_fields.EventId,
-            AttendeeStatus: bundle.trigger_fields.Status
-        };
-        bundle.request.method = 'POST';
-        bundle.request.data = JSON.stringify(data);
-        return bundle.request;
-    },
-
-    apsis_get_active_events_post_poll: function (bundle) {
-        // Working
-        var response = JSON.parse(bundle.response.content);
-        var result = response.Result;
-        if (result.length > 1) {
-            throw new ErrorException('There are no active events available on your APSIS account.');
-        }
-        var events = [];
-        for (var e in result) {
-            var newEvent = { Id: result[e].Id, Name: result[e].Name };
-            events.push(newEvent);
-        }
-        bundle.response.content = events;
-        return bundle.response;
-    },
-
-    apsis_get_active_events_pre_poll: function (bundle) {
-        // Working
-        var data = { ExcludeDisabled: "true" };
-        bundle.request.method = 'POST';
-        bundle.request.data = JSON.stringify(data);
-        return bundle.request;
-    },
-
-    apsis_create_subscriber_pre_write: function (bundle) {
-        // Working
-        var actionFields = bundle.action_fields;
-        var data = {};
-        var demDataFields = [];
-        for (var prop in actionFields) {
-            if (actionFields.hasOwnProperty(prop)) {
-                if (prop === "Key") {
-                    var keys = actionFields[prop];
-                    var index = 0;
-                    for (var pr in keys) {
-                        if (demDataFields.length - 1 < index) {
-                            throw new ErrorException('The number of Demographic Data fields can not be greater than the number of values.');
-                        }
-                        demDataFields[pr].Key = keys[pr];
-                        index++;
-                    }
-                }
-                else if (prop === "Value") {
-                    var values = actionFields[prop];
-                    for (var p in values) {
-                        var newObj = { Value: values[p], Key: "" };
-                        demDataFields.push(newObj);
-                    }
-                }
-                else {
-                    data[prop] = actionFields[prop];
-                }
-            }
-        }
-        if (demDataFields.length > 0) {
-            for (var i = 0; i < demDataFields.length; i++) {
-                if (demDataFields[i].Key === "") {
-                    throw new ErrorException('The number of values can not be greater than the number of Demographic Data fields.');
-                }
-            }
-            data.demDataFields = demDataFields;
-        }
-        bundle.request.data = JSON.stringify(data);
-        return bundle.request;
-    },
-
-    apsis_get_demographic_data_post_poll: function (bundle) {
-        const responseObj = JSON.parse(bundle.response.content);
-        const resultObj = responseObj.Result.Demographics;
-
-        return Object.assign({}, bundle.response, {
-            content: resultObj.filter((result) => result.Key !== ""),
-        })
-    },
-
-    apsis_get_subscriber_id_pre_write(bundle) {
-        return deepAssign({}, bundle.request, {
-            data: `"${bundle.action_fields_full.Email}"`,
-        });
-    },
-
-    apsis_unsubscribe_pre_write: function (bundle) {
-        var data = [{
-            MailinglistId: 0,
-            Reason: 'Moved to OptOutAll via Zapier',
-            SendQueueId: 0,
-            SubscriberId: bundle.action_fields_full.SubscriberId,
-        }];
-
-        return Object.assign({}, bundle.request, {
-            data: JSON.stringify(data),
-        });
-    },
-
-    apsis_incoming_sms_post_poll(bundle) {
-        const smskeyword = bundle.trigger_fields.Keyword;
-        const results = JSON.parse(bundle.response.content, (key, value) => {
-            if (key === 'SmsResponse') {
-                // @TODO
-                // What if keyword is the last char, or is followed by a dot etc?
-                return value.replace(smskeyword + " ", '');
-            }
-            return value;
-        });
-
-        return results;
-    },
-
-    apsis_get_mailinglist_pre_poll(bundle) {
-        return deepAssign({}, bundle.request, {
-            method: 'POST',
-        });
+      }
     }
+    if (demDataFields.length > 0) {
+      for (var i = 0; i < demDataFields.length; i++) {
+        if (demDataFields[i].Key === "") {
+          throw new ErrorException('The number of values can not be greater than the number of Demographic Data fields.');
+        }
+      }
+      data.demDataFields = demDataFields;
+    }
+    bundle.request.data = JSON.stringify(data);
+    return bundle.request;
+  },
+
+  remove_subscriber_from_optoutall_post_write: function (bundle) {
+    // Working
+    var response = JSON.parse(bundle.response.content);
+    if (response.Message === 'Subscriber e-mail address does not exist on the Opt-out all list') {
+      bundle.response.status_code = 200;
+    }
+    console.log(response.Message);
+    return bundle.response;
+  },
+
+  remove_subscriber_from_optoutall_pre_write: function (bundle) {
+    // Working
+    bundle.request.method = 'DELETE';
+    var data = bundle.action_fields_full.email;
+    bundle.request.data = JSON.stringify(data);
+    return bundle.request;
+  },
+
+
+  apsis_new_mailinglist_subscriber_post_poll: function (bundle) {
+    // Working
+    var data = JSON.parse(bundle.response.content);
+    if (data.Code != 1) {
+      throw new ErrorException('Unable to retrieve the subscribers on the mailinglist');
+    }
+    else {
+      var pages = parseInt(data.Result.TotalPages, 10);
+      console.log(typeof (pages));
+      if (pages > 1) {
+        var request = {
+          'method': 'GET',
+          'url': 'https://se.api.anpdm.com:8443/v1/mailinglists/' + bundle.trigger_fields.MailinlistID + '/subscribers/' + pages + '/100',
+          'params': {},
+          'headers': {
+            'accept': 'application/json',
+            'content-type': 'application/json; charset=utf-8'
+          },
+          'auth': bundle.request.auth,
+          'data': null
+        };
+
+        var response = JSON.parse(z.request(request));
+        data = response.content;
+      }
+      bundle.response.content = Subscribers;
+
+      return bundle.response;
+    }
+  },
+
+  apsis_new_mailinglist_subscriber_pre_poll: function (bundle) {
+    // Working
+    return bundle.request;
+  },
+
+  apsis_update_event_attendee_pre_write: function (bundle) {
+    // Working
+    bundle.request.method = 'PUT';
+    var data = bundle.action_fields_full.Status;
+    bundle.request.data = JSON.stringify(data);
+    return bundle.request;
+  },
+
+  apsis_find_attendee_pre_search: function (bundle) {
+    // Working
+    var data = { EventId: bundle.search_fields.eventId };
+    bundle.request.method = 'POST';
+    bundle.request.data = JSON.stringify(data);
+    return bundle.request;
+  },
+
+  apsis_get_event_attendee_pre_poll: function (bundle) {
+    // Working
+    var data = {
+      EventId: bundle.trigger_fields.EventId,
+      AttendeeStatus: bundle.trigger_fields.Status
+    };
+    bundle.request.method = 'POST';
+    bundle.request.data = JSON.stringify(data);
+    return bundle.request;
+  },
+
+  apsis_get_active_events_post_poll: function (bundle) {
+    // Working
+    var response = JSON.parse(bundle.response.content);
+    var result = response.Result;
+    if (result.length > 1) {
+      throw new ErrorException('There are no active events available on your APSIS account.');
+    }
+    var events = [];
+    for (var e in result) {
+      var newEvent = { Id: result[e].Id, Name: result[e].Name };
+      events.push(newEvent);
+    }
+    bundle.response.content = events;
+    return bundle.response;
+  },
+
+  apsis_get_active_events_pre_poll(bundle) {
+    return Object.assign({}, bundle.request, {
+      method: 'POST',
+      data: JSON.stringify({ ExcludeDisabled: 'true' }),
+    });
+  },
+
+  apsis_create_subscriber_pre_write: function (bundle) {
+    // Working
+    var actionFields = bundle.action_fields;
+    var data = {};
+    var demDataFields = [];
+    for (var prop in actionFields) {
+      if (actionFields.hasOwnProperty(prop)) {
+        if (prop === "Key") {
+          var keys = actionFields[prop];
+          var index = 0;
+          for (var pr in keys) {
+            if (demDataFields.length - 1 < index) {
+              throw new ErrorException('The number of Demographic Data fields can not be greater than the number of values.');
+            }
+            demDataFields[pr].Key = keys[pr];
+            index++;
+          }
+        }
+        else if (prop === "Value") {
+          var values = actionFields[prop];
+          for (var p in values) {
+            var newObj = { Value: values[p], Key: "" };
+            demDataFields.push(newObj);
+          }
+        }
+        else {
+          data[prop] = actionFields[prop];
+        }
+      }
+    }
+    if (demDataFields.length > 0) {
+      for (var i = 0; i < demDataFields.length; i++) {
+        if (demDataFields[i].Key === "") {
+          throw new ErrorException('The number of values can not be greater than the number of Demographic Data fields.');
+        }
+      }
+      data.demDataFields = demDataFields;
+    }
+    bundle.request.data = JSON.stringify(data);
+    return bundle.request;
+  },
+
+  apsis_get_demographic_data_post_poll(bundle) {
+    const responseObj = JSON.parse(bundle.response.content);
+    const resultObj = responseObj.Result.Demographics;
+
+    return Object.assign({}, bundle.response, {
+      content: resultObj.filter(result => result.Key !== ''),
+    });
+  },
+
+  apsis_get_subscriber_id_pre_write(bundle) {
+    return deepAssign({}, bundle.request, {
+      data: `"${bundle.action_fields_full.Email}"`,
+    });
+  },
+
+  apsis_unsubscribe_pre_write(bundle) {
+    return Object.assign({}, bundle.request, {
+      data: JSON.stringify([{
+        MailinglistId: 0,
+        Reason: 'Moved to OptOutAll via Zapier',
+        SendQueueId: 0,
+        SubscriberId: bundle.action_fields_full.SubscriberId,
+      }]),
+    });
+  },
+
+  apsis_incoming_sms_post_poll(bundle) {
+    const smskeyword = bundle.trigger_fields.Keyword;
+    const results = JSON.parse(bundle.response.content, (key, value) => {
+      if (key === 'SmsResponse') {
+        // @TODO
+        // What if keyword is the last char, or is followed by a dot etc?
+        return value.replace(`${smskeyword} `, '');
+      }
+      return value;
+    });
+
+    return results;
+  },
+
+  apsis_get_mailinglist_pre_poll(bundle) {
+    return deepAssign({}, bundle.request, {
+      method: 'POST',
+    });
+  },
 };
 
 module.exports = Zap;
--- zapier.spec.js
+++ zapier.spec.js
@@ -2,40 +2,63 @@ const test = require('tape');
 const deepAssign = require('deep-assign');
 const zap = require('./zapier.js');
 
-const testBundle = {
-  action_fields_full: {
-    Email: 'test@apsis.com',
-    SubscriberId: 123,
-  },
-  name: 'test',
-  request: {
-    id: 0,
-    method: 'GET',
-  },
-  response: {
-    content: JSON.stringify({
+function getTestBundle() {
+  return {
+    action_fields_full: {
+      Email: 'test@apsis.com',
+      SubscriberId: 123,
+    },
+    name: 'test',
+    request: {
       id: 0,
-      SmsResponse: 'A keyword for sms',
-      Result: {
-        Demographics: [
-          'first_name',
-          'last_name',
-          'age',
-        ],
-      },
-    }),
-  },
-  trigger_fields: {
-    Keyword: 'keyword',
-  },
-};
+      method: 'GET',
+    },
+    response: {
+      content: JSON.stringify({
+        id: 0,
+        SmsResponse: 'A keyword for sms',
+        Result: {
+          Demographics: [
+            'first_name',
+            'last_name',
+            'age',
+          ],
+        },
+      }),
+    },
+    trigger_fields: {
+      Keyword: 'keyword',
+    },
+  };
+}
 
-test('apsis_get_demographic_data_post_poll', (t) => {
-  const sut = zap.apsis_get_demographic_data_post_poll;
+function getAndTestMethod(t, name) {
+  const sut = zap[name];
   t.equal(typeof sut, 'function');
 
-  const result = sut(testBundle);
-  t.notEqual(result, testBundle.response);
+  return sut;
+}
+
+
+test('apsis_get_active_events_pre_poll', (t) => {
+  const sut = getAndTestMethod(t, 'apsis_get_active_events_pre_poll');
+  const result = sut(getTestBundle());
+  const expectedResult = deepAssign({}, getTestBundle().request, {
+    method: 'POST',
+    data: JSON.stringify({
+      ExcludeDisabled: 'true',
+    }),
+  });
+
+  t.looseEqual(result, expectedResult, 'Should update method and content data.');
+  t.end();
+});
+
+test('apsis_get_demographic_data_post_poll', (t) => {
+  const sut = getAndTestMethod(t, 'apsis_get_demographic_data_post_poll');
+
+  const result = sut(getTestBundle());
+  t.notEqual(result, getTestBundle().response);
   t.looseEqual(result, {
     content: ['first_name', 'last_name', 'age'],
   });
@@ -43,23 +66,22 @@ test('apsis_get_demographic_data_post_poll', (t) => {
 });
 
 test('apsis_get_subscriber_id_pre_write', (t) => {
-  const sut = zap.apsis_get_subscriber_id_pre_write;
-  t.equal(typeof sut, 'function');
-
+  const sut = getAndTestMethod(t, 'apsis_get_subscriber_id_pre_write');
+  const response = sut(getTestBundle());
   const expectedResponse = {
     id: 0,
     method: 'GET',
     data: '"test@apsis.com"',
   };
-  const response = sut(testBundle);
-  t.notEqual(response, testBundle.request);
+
+  t.notEqual(response, getTestBundle().request);
   t.looseEqual(response, expectedResponse, 'Should have an updated response.data object');
 
   t.end();
 });
 
 test('apsis_unsubscribe_pre_write', (t) => {
-  const sut = zap.apsis_unsubscribe_pre_write;
+  const sut = getAndTestMethod(t, 'apsis_unsubscribe_pre_write');
   t.equal(typeof sut, 'function');
 
   const expectedResponse = {
@@ -73,34 +95,34 @@ test('apsis_unsubscribe_pre_write', (t) => {
     }]),
   };
 
-  const response = sut(testBundle);
-  t.notEqual(response, testBundle.request);
+  const response = sut(getTestBundle());
+  t.notEqual(response, getTestBundle().request);
   t.looseEqual(response, expectedResponse, 'Should return a request object with appended data');
   t.end();
 });
 
 test('apsis_incoming_sms_post_poll', (t) => {
-  const sut = zap.apsis_incoming_sms_post_poll;
+  const sut = getAndTestMethod(t, 'apsis_incoming_sms_post_poll');
   t.equal(typeof sut, 'function');
 
-  const response = sut(testBundle);
-  const expectedResponse = deepAssign({}, JSON.parse(testBundle.response.content), {
+  const response = sut(getTestBundle());
+  const expectedResponse = deepAssign({}, JSON.parse(getTestBundle().response.content), {
     SmsResponse: 'A for sms',
   });
 
-  t.notEqual(response, testBundle.response);
+  t.notEqual(response, getTestBundle().response);
   t.looseEqual(response, expectedResponse, 'Should remove keyword from response.content');
   t.end();
 });
 
 test('apsis_get_mailinglist_pre_poll', (t) => {
-  const sut = zap.apsis_get_mailinglist_pre_poll;
+  const sut = getAndTestMethod(t, 'apsis_get_mailinglist_pre_poll');
   t.equal(typeof sut, 'function', 'Should be a function');
 
-  const response = sut(testBundle);
+  const response = sut(getTestBundle());
 
-  t.notEqual(response, testBundle.request, 'Should not return the same object');
-  t.equal(response.id, testBundle.request.id, 'Should not touch other props');
+  t.notEqual(response, getTestBundle().request, 'Should not return the same object');
+  t.equal(response.id, getTestBundle().request.id, 'Should not touch other props');
   t.equal(response.method, 'POST', 'Should update the method to POST');
   t.end();
 });
